20c4e0b4304e6d04d3e7d071558df82880c7c13d
Jessica <jessica@Jessicas-MacBook-Air.local>
2022-01-16 18:19:01 -0500

    changed database

--- api.py
+++ api.py
@@ -1,21 +1,44 @@
-from flask import Flask, render_template
+from multiprocessing import connection
+from flask import Flask, render_template, request,redirect,url_for
 
 app = Flask(__name__)
+
+
 
 import sqlite3
 from sqlite3 import Error
 
-def create_connection(recipes.db):
+def create_connection(path):
     connection = None
     try:
-        connection = sqlite3.connect(recipes.db)
-        print("Connection to SQLite DB successful")
+        connection = sqlite3.connect(path)
+        print("Connection to RECIPES DB successful")
     except Error as e:
-        print(f"The error '{e}' occurred")
+        print(f"The error {e} occurred")
 
     return connection
 
 
+def execute_query(connection, query, values= ()):
+    cursor = connection.cursor()
+    print(values)
+    try:
+        if len(values) > 0:
+            cursor.execute(query,values)
+        else:
+            cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except Error as e:
+        print(f"The error{e} occurred")
+
+create_recipesflask_table = """
+CREATE TABLE IF NOT EXISTS recipes (
+  id INTEGER PRIMARY KEY,
+  image BLOB NOT NULL,
+  link INTEGER NOT NULL
+);
+"""
 
 
 @app.route('/')
@@ -26,9 +49,14 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/recipe/', methods=['POST', 'GET'])
+def create_recipe():
+    form =request.form
+    #  print(form)
+    # title = request.form.get('title')
+    # print(request.form.get('title'))
+    return redirect(url_for('home'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
 recipes = [
         {
@@ -38,7 +66,13 @@ recipes = [
         },
         {
             "title": "Pasta Bolognese",
-            "image": "https://www.jocooks.com/wp-content/uploads/2018/01/spaghetti-bolognese-1-3-500x375.jpg"
+            "image": "https://www.jocooks.com/wp-content/uploads/2018/01/spaghetti-bolognese-1-3-500x375.jpg",
             "link": "https://www.onceuponachef.com/recipes/pasta-bolognese.html"
         }
     ]
+
+if __name__ == '__main__':
+
+    conn =create_connection("recipesflask.db")
+    execute_query(conn,create_recipesflask_table)
+    app.run(debug=True)
